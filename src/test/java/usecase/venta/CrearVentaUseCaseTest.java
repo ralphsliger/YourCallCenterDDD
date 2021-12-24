f877d0e31b556a9bf6e8ae6b709e0a527809f71a
Ralph Sliger <ralphsliger1@gmail.com>
2021-12-24 03:23:13 -0500

    Venta creada test

---
+++ src/test/java/usecase/venta/CrearVentaUseCaseTest.java
@@ -0,0 +1,55 @@
+package usecase.venta;
+
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import domain.venta.Asesor;
+import domain.venta.Cliente;
+import domain.venta.command.CrearVenta;
+import domain.venta.events.VentaCreada;
+import domain.venta.values.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrearVentaUseCaseTest {
+
+    private CrearVentaUseCase ventaUseCase;
+    private DomainEventRepository repository;
+
+    @BeforeEach
+    public void config(){
+        ventaUseCase = new CrearVentaUseCase();
+        repository = mock(DomainEventRepository.class);
+        ventaUseCase.addRepository(repository);
+    }
+
+    @Test
+    public void generaVenta(){
+        AsesorID idAsesor = AsesorID.of("MB");
+        Nombre nombre = new Nombre("Maria");
+
+        ClienteID idCliente = ClienteID.of("300123456");
+        Identificacion identificacion = new Identificacion("xx", "Cedula");
+        Comprobante comprobante = new Comprobante("Recibo luz mes enero", "Recibo luz");
+        NumeroContacto contacto = new NumeroContacto("300123456");
+        Downpayment downpayment = new Downpayment(1000.0, "cash");
+
+
+        Asesor asesor = new Asesor(idAsesor, nombre);
+        Cliente cliente = new Cliente(idCliente, identificacion,comprobante, contacto, downpayment);
+
+        var command = new CrearVenta(VentaID.of("DMLEAD"), asesor, cliente);
+        var response = UseCaseHandler.getInstance().setIdentifyExecutor("DMLEAD").syncExecutor(ventaUseCase, new RequestCommand<>(command)).orElseThrow();
+        var events = response.getDomainEvents();
+        VentaCreada ventaCreada = (VentaCreada) events.get(0);
+
+
+        Assertions.assertEquals("Maria", ventaCreada.getAsesor().Nombre().value());
+
+    }
+
+}
